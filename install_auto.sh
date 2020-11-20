684b0465255c45ce731790620fcfdb1103527e59
sunny5156 <sunny5156@qq.com>
2020-11-20 20:51:00 +0800

    add

---
+++ install_auto.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+if [ -e /tmp/proxy ]; then
+    rm -rf /tmp/proxy
+fi
+mkdir /tmp/proxy
+cd /tmp/proxy
+wget https://github.com/sunny5156/goproxyx/releases/download/v6.2/proxy-linux-amd64.tar.gz
+
+# #install proxy
+tar zxvf proxy-linux-amd64.tar.gz
+cp proxy /usr/bin/
+chmod +x /usr/bin/proxy
+if [ ! -e /etc/proxy ]; then
+    mkdir /etc/proxy
+    cp blocked /etc/proxy
+    cp direct  /etc/proxy
+fi
+
+if [ ! -e /etc/proxy/proxy.crt ]; then
+    cd /etc/proxy/
+    proxy keygen -C proxy >/dev/null 2>&1 
+fi
+rm -rf /tmp/proxy
+echo "install done"
+proxy help
---
+++ sdk/windows-linux/sdk.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"C"
+
+	sdk "github.com/sunny5156/goproxyx/sdk/android-ios"
+)
+
+//export Start
+func Start(serviceID *C.char, serviceArgsStr *C.char) (errStr *C.char) {
+	return C.CString(sdk.Start(C.GoString(serviceID), C.GoString(serviceArgsStr)))
+}
+
+//export Stop
+func Stop(serviceID *C.char) {
+	sdk.Stop(C.GoString(serviceID))
+}
+
+//export Version
+func Version() (ver *C.char) {
+	return C.CString(sdk.Version())
+}
+
+func main() {
+}
